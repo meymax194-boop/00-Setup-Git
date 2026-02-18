989cc801ed3732a21591568d827c612278bb65b8
Eman <meymax194l@example.com>
2026-02-18 22:42:51 +0300

    Fix pylint and implement my_name_is

--- today.py
+++ today.py
@@ -1,9 +1,13 @@
+"""
+Lab 00-Setup-Git: simple functions for practice.
+"""
 # pylint: disable=C0116,C0303
 
 
 # $CHALLENGIFY_BEGIN
 def my_name_is():
  
-    pass
+    return "eman yassen"
+
 
 # $CHALLENGIFY_END
